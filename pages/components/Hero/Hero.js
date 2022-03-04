a188e6a9c8ccaecce48e12e2e3c26015ede6208b
Mufaqar Islam <mufaqar@gmail.com>
2022-03-04 21:01:48 +0500

    Lint code

--- pages/components/Hero/Hero.js
+++ pages/components/Hero/Hero.js
@@ -1,32 +1,34 @@
-import React from 'react'
+import React from 'react';
 import { FaMapMarkerAlt } from 'react-icons/fa';
 import { BsFillClockFill } from 'react-icons/bs';
-import Image from 'next/image'
-import heropic from '../../../public/images/hero.png'
+import Image from 'next/image';
+import heropic from '../../../public/images/hero.png';
 
 export default function Hero() {
   return (
-    <div className='container hero flex v_align space_between'>
-        <div className="left">
-            <h1>Hungry? You're in the right place</h1>
-            <div className='hero-find-food'>
-                <div className='find-input'>
-                    <div style={{marginTop:4}}><FaMapMarkerAlt size="1.3rem" color='#ce1a26' /></div>
-                    <input type="text" placeholder='Enter delivery address'/>
-                </div>
-                <div className='find-input inp2'>
-                    <BsFillClockFill size="1.3rem" color='#ce1a26' />
-                    <input type="text" placeholder='Delivery Time'/>
-                </div>
-                <button>Find Food</button>
+    <div className="container hero flex v_align space_between">
+      <div className="left">
+        <h1>Hungry? Youre in the right place</h1>
+        <div className="hero-find-food">
+          <div className="find-input">
+            <div style={{ marginTop: 4 }}>
+              <FaMapMarkerAlt size="1.3rem" color="#ce1a26" />
             </div>
-            <p>Sign In for your recent addresses</p>
+            <input type="text" placeholder="Enter delivery address" />
+          </div>
+          <div className="find-input inp2">
+            <BsFillClockFill size="1.3rem" color="#ce1a26" />
+            <input type="text" placeholder="Delivery Time" />
+          </div>
+          <button>Find Food</button>
         </div>
-        <div className="right">
-            <figure>
-                <Image src={heropic} alt='hero-burrgur' />
-            </figure>
-        </div>
+        <p>Sign In for your recent addresses</p>
+      </div>
+      <div className="right">
+        <figure>
+          <Image src={heropic} alt="hero-burrgur" />
+        </figure>
+      </div>
     </div>
-  )
+  );
 }
--- pages/components/banner/Banner.js
+++ pages/components/banner/Banner.js
@@ -1,14 +1,12 @@
-import React from 'react'
+import React from 'react';
 
-export default function ({pic}) {
-  console.log(pic.src);
+export default function Banner({ pic }) {
   return (
-    <div className='banner_wrapper' 
-        style={{
+    <div
+      className="banner_wrapper"
+      style={{
         backgroundImage: `linear-gradient(to right, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${pic.src})`,
       }}
-    >
-
-    </div>
-  )
+    ></div>
+  );
 }
